d9c39e73ebb66dcb876108ecc933ebb397a7e72c
ar3secchim <renarasecchim@gmail.com>
2023-10-24 09:43:29 -0300

    chore: name package force push

--- src/main/java/com/ada/order/controller/dto/CurrencyResponse.java
+++ src/main/java/com/ada/order/controller/dto/CurrencyResponse.java
@@ -1,4 +1,4 @@
-package com.ada.order.Controller.dto;
+package com.ada.order.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
@@ -8,4 +8,3 @@ public class CurrencyResponse {
     @JsonProperty("high")
     private double value;
 }
-
