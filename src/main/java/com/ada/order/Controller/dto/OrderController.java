746d7a77c3cb6db244ab4cd14f46f0fb62d7034b
Ar3secchim <renarasecchim@gmail.com>
2023-10-27 10:29:59 -0300

    chore: update orderController

---
+++ src/main/java/com/ada/order/Controller/dto/OrderController.java
@@ -0,0 +1,27 @@
+package com.ada.order.controller.dto;
+
+import com.ada.order.controller.dto.order.OrderRequest;
+import com.ada.order.controller.dto.order.OrderResponse;
+import com.ada.order.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.net.URI;
+
+@Controller
+public class OrderController {
+    @Autowired
+    OrderService orderService;
+
+    @PostMapping("/order")
+    public ResponseEntity<OrderResponse> createOrder(@RequestBody OrderRequest orderRequest){
+      OrderResponse order =  orderService.create(orderRequest);
+      return ResponseEntity
+              .created(URI.create("/"+order.getId()))
+              .body(order);
+    }
+}
